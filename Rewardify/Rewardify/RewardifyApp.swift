bb57ae554abd03fdcca52f7273202cfcc6c145bd
wiseguy16 <gergusa04@gmail.com>
2023-01-11 12:40:15 -0500

    setting up project

---
+++ Rewardify/Rewardify/RewardifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RewardifyApp.swift
+//  Rewardify
+//
+//  Created by Gregory Weiss on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RewardifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
